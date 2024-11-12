37e76ebeab317296eddfb06065076e009dac82fa
Vyachaz <vyachaz@mail.ru>
2024-11-12 10:41:08 +0500

    GameBehavior

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
+    public string lableText = "Collect all 4 items and win your freedom!";
+    public int maxItems = 4;
+    public bool showWinScreen = false;
+
     private int _itemsCollected = 0;
 
     public int Items
@@ -17,7 +21,18 @@ public class GameBehavior : MonoBehaviour
         {
             _itemsCollected = value;
             Debug.LogFormat("Items: {0}", _itemsCollected);
+
+            if (_itemsCollected >= maxItems)
+            {
+                lableText = "You've found all the items!";
+                showWinScreen = true;
+            }
+            else { 
+                lableText = "Item found? only " + (maxItems - _itemsCollected) +
+                    " more to go!";
+            }
         }
+
     }
 
 
@@ -36,7 +51,25 @@ public class GameBehavior : MonoBehaviour
         }
     }
 
-    
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerHP);
+
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected );
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300,50),
+            lableText);
+
+        if (showWinScreen) 
+        {
+            if(GUI.Button(new Rect(Screen.width/2 -100, Screen.height/2 - 50, 200, 100), "YOU WON!"))
+            {
+
+            }
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
